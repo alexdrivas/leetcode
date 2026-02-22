f63b8244444a3ca87d96bbacf25a182493746899
Alexandros Drivas <aadrivas@gmail.com>
2026-02-21 16:40:26 -0800

    Missing Number 268

---
+++ MissingNumber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+        if 0 not in nums_set: return 0
+
+        nums.sort() #we sort to prepare indexing, ensuring index 0 contains 0. 
+        for i in range(len(nums)):
+            #print(i)
+            if nums[i]+1 in nums_set: # O(n) look up time 
+                pass
+            else:
+                return i+1 # return missing value
